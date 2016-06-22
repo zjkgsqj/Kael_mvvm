a4a114a9831bb7efd69acfbbbb632bff4f80b23f
zjkgsqj <zjkgsqj@yeah.net>
2016-06-22 17:53:30 +0800

    first commit

---
+++ app/src/main/java/com/ftd/keal/keal_mvvm/viewmodel/MainViewModel.java
@@ -0,0 +1,26 @@
+package com.ftd.keal.keal_mvvm.viewmodel;
+
+import android.content.Context;
+import android.databinding.ObservableField;
+
+/**
+ * Created by sdhuang on 16/6/22 11:06.
+ */
+public class MainViewModel implements ViewModel{
+    private Context context;
+    public final ObservableField<String> mainText;
+    public MainViewModel(Context context) {
+        this.context = context;
+        this.mainText = new ObservableField<>();
+        setText();
+    }
+
+    @Override
+    public void destroy() {
+        context = null;
+    }
+
+    private void setText(){
+        mainText.set("Keal MVVM");
+    }
+}
---
+++ app/src/main/java/com/ftd/keal/keal_mvvm/viewmodel/ViewModel.java
@@ -0,0 +1,8 @@
+package com.ftd.keal.keal_mvvm.viewmodel;
+
+/**
+ * Created by sdhuang on 16/6/22 12:06.
+ */
+public interface ViewModel {
+    void destroy();
+}
